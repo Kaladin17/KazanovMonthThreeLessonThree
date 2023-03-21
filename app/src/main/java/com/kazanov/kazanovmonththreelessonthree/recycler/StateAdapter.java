0c54c824c8a4acd27867cf2976d0bbf325c2357a
Kaladin17 <dekazanov8@gmail.com>
2023-03-21 18:02:12 +0600

    changes

--- app/src/main/java/com/kazanov/kazanovmonththreelessonthree/recycler/StateAdapter.java
+++ app/src/main/java/com/kazanov/kazanovmonththreelessonthree/recycler/StateAdapter.java
@@ -1,6 +1,5 @@
 package com.kazanov.kazanovmonththreelessonthree.recycler;
 
-import static android.os.Build.VERSION_CODES.R;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -12,12 +11,14 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.kazanov.kazanovmonththreelessonthree.R;
+
 import java.util.List;
 
 public class StateAdapter extends RecyclerView.Adapter<StateAdapter.CountryViewHolder> {
 
     private final LayoutInflater inflater;
-    private final List <State> states;
+    private final List<State> states;
 
     public StateAdapter (Context context, List<State> states) {
         this.states = states;
@@ -27,12 +28,12 @@ public class StateAdapter extends RecyclerView.Adapter<StateAdapter.CountryViewH
     @NonNull
     @Override
     public StateAdapter.CountryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = inflater.inflate(R.layout.item_countries, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_countries, parent, false);
         return new CountryViewHolder (view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull StateAdapter.CountryViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull CountryViewHolder holder, int position) {
 
         State state = states.get(position);
         holder.flagView.setImageResource(state.getFlag());
@@ -50,8 +51,8 @@ public class StateAdapter extends RecyclerView.Adapter<StateAdapter.CountryViewH
         CountryViewHolder(View view){
             super(view);
             flagView = view.findViewById(R.id.iv_flag);
-            countryView = view.findViewById(R.id.name);
-            capitalView = view.findViewById(R.id.capital);
+            countryView = view.findViewById(R.id.tv_country);
+            capitalView = view.findViewById(R.id.tv_capital);
         }
 
     }
